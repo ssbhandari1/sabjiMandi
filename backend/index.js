7fbfa03046b32c594071d689080c5dde4f8295e6
santosh <santoshbirkhal@gmail.com>
2023-09-12 16:32:18 +0530

    atlas

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express=require('express')
 const cors=require('cors')
 const mongoose=require('mongoose')
 const UserRouter = require('./routes/User')
 const ProductRouter = require('./routes/Products')
-
+const PORT =process.env.PORT || 4001
+const DB = process.env.DATABASE
 const app=express()
 app.use(express.json())
 app.use(cors())
@@ -11,12 +13,12 @@ app.use(cors())
 app.use('/',UserRouter)
 app.use('/product',ProductRouter)
 
-mongoose.connect('mongodb://127.0.0.1:27017/mandi',  {
+mongoose.connect(DB,  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
 
 
-  app.listen('4001',()=>{
+  app.listen(PORT,()=>{
     console.log('Server Started')
   })
